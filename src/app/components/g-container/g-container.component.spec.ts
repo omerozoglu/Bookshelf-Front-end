3383110332533fa08209c966b706a991b1ff70f7
rasenshuriken <omerozoglu.1@gmail.com>
2023-01-14 00:48:58 +0300

    feat: Create container component.

---
+++ src/app/components/g-container/g-container.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GContainerComponent } from './g-container.component';
+
+describe('GContainerComponent', () => {
+  let component: GContainerComponent;
+  let fixture: ComponentFixture<GContainerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GContainerComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/g-container/g-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-g-container',
+  templateUrl: './g-container.component.html',
+  styleUrls: ['./g-container.component.scss']
+})
+export class GContainerComponent {
+
+}
