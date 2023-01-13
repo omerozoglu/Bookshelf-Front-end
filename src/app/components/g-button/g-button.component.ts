f3d88c020c5ff990b26396365e153c79b306b0e2
rasenshuriken <omerozoglu.1@gmail.com>
2023-01-14 00:49:06 +0300

    feat: Create button component.

---
+++ src/app/components/g-button/g-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-g-button',
+  templateUrl: './g-button.component.html',
+  styleUrls: ['./g-button.component.scss']
+})
+export class GButtonComponent {
+
+}
