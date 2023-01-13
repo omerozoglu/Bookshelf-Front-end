66401c90b02895dfea9afebb3e039003b8e8044f
rasenshuriken <omerozoglu.1@gmail.com>
2023-01-14 00:47:44 +0300

    feat: Create navigation component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GButtonComponent } from './components/g-button/g-button.component';
+import { GTableComponent } from './components/g-table/g-table.component';
+import { GFilterComponent } from './components/g-filter/g-filter.component';
+import { GInputComponent } from './components/g-input/g-input.component';
+import { GContainerComponent } from './components/g-container/g-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    FooterComponent,
+    GButtonComponent,
+    GTableComponent,
+    GFilterComponent,
+    GInputComponent,
+    GContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    AuthModule,
+    ProfileModule
   ],
   providers: [],
   bootstrap: [AppComponent]
